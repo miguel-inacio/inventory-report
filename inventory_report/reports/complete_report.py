e616fb446f3dd7269e798089fc131193f9d812e6
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-04 20:51:28 -0300

    fix: corrige quebras de linha

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,7 +6,7 @@ class CompleteReport(SimpleReport):
     def generate(cls, report: list[dict]) -> str:
         base_str = super().generate(report)
         companies_quantities = cls.get_most_common_company(report)
-        companies_and_quantities = "\n".join(
+        companies_and_quantities = "".join(
             f"- {tup[0]}: {tup[1]}\n" for tup in companies_quantities
         )
         return (
