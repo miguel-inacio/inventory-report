0c33d13446d24d832c1d70370d6332d2234701de
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-04 15:36:06 -0300

    test: adiciona teste para criação de produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito
+    produto_test = Product(
+        44,
+        "Dunder Mifflin",
+        "Papel A4",
+        "2005-03-24",
+        "2013-05-16",
+        201,
+        "Coloque na TV e ria bastante",
+    )
+
+    assert produto_test.id == 44
+    assert produto_test.nome_do_produto == "Dunder Mifflin"
+    assert produto_test.nome_da_empresa == "Papel A4"
+    assert produto_test.data_de_fabricacao == "2005-03-24"
+    assert produto_test.data_de_validade == "2013-05-16"
+    assert produto_test.numero_de_serie == 201
+    assert (
+        produto_test.instrucoes_de_armazenamento
+        == "Coloque na TV e ria bastante"
+    )
