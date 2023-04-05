1a05df54f8e09ca0fad12c22a4b18eb11b3cf47b
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-05 10:37:50 -0300

    test: adiciona teste para relatório de produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto_test = Product(
+        44,
+        "Papel A4",
+        "Dunder Mifflin",
+        "2005-03-24",
+        "2013-05-16",
+        201,
+        "em lugar seco",
+    )
+
+    representation = str(produto_test.__repr__())
+
+    assert representation == (
+        "O produto Papel A4"
+        " fabricado em 2005-03-24"
+        " por Dunder Mifflin com validade"
+        " até 2013-05-16"
+        " precisa ser armazenado em lugar seco."
+    )
