c4c30534d46dc228bd319b9d63332a2f639dec8e
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-05 11:56:49 -0300

    fix: corrige uso de lib xmltodict

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -69,3 +69,50 @@ class Inventory:
         report = cls.read_report_file(path)
         type_class = type.capitalize()
         return eval(type_class).get_report(report)
+
+
+# from abc import ABC, abstractmethod
+# from inventory_report.reports.simple_report import SimpleReport
+# from inventory_report.reports.complete_report import CompleteReport
+# from inventory_report.importer.csv_importer import CsvImporter
+# from inventory_report.importer.json_importer import JsonImporter
+# from inventory_report.importer.xml_importer import XmlImporter
+
+
+# class ReportTypeStrategy(ABC):
+#     @abstractmethod
+#     def get_report(cls, report):
+#         raise NotImplementedError
+
+
+# class Simples(ReportTypeStrategy):
+#     @classmethod
+#     def get_report(cls, report):
+#         return SimpleReport.generate(report)
+
+
+# class Completo(ReportTypeStrategy):
+#     @classmethod
+#     def get_report(cls, report):
+#         return CompleteReport.generate(report)
+
+
+# importers = [CsvImporter, JsonImporter, XmlImporter]
+
+
+# class Inventory:
+#     @classmethod
+#     def read_report_file(cls, path: str) -> list[dict]:
+#         importer_strategy = path.split(".")[-1].capitalize() + "Importer"
+#         used_importer = [
+#             importer for importer
+#             in importers if importer == importer_strategy
+#         ]
+#         formatted_file = used_importer[0].read(path)
+#         return formatted_file
+
+#     @classmethod
+#     def import_data(cls, path: str, type: str):
+#         report = cls.read_report_file(path)
+#         type_class = type.capitalize()
+#         return eval(type_class).get_report(report)
