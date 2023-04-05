81d48e58b727e1bff1b98a97c1d68867ec7f1198
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-05 14:53:48 -0300

    fix: simplifica comparação de classe e string

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,42 +1,9 @@
 from abc import ABC, abstractmethod
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-import csv
-import json
-import xmltodict
-
-
-class ReportReaderStrategy(ABC):
-    @abstractmethod
-    def read(cls, path):
-        raise NotImplementedError
-
-
-class CSVFileReader(ReportReaderStrategy):
-    @classmethod
-    def read(cls, path):
-        with open(path) as file:
-            read_file = csv.DictReader(file)
-            formatted_file = [row for row in read_file]
-            return formatted_file
-
-
-class JSONFileReader(ReportReaderStrategy):
-    @classmethod
-    def read(cls, path):
-        with open(path) as file:
-            read_file = file.read()
-            formatted_file = json.loads(read_file)
-            return formatted_file
-
-
-class XMLFileReader(ReportReaderStrategy):
-    @classmethod
-    def read(cls, path):
-        with open(path) as file:
-            read_file = file.read()
-            formatted_file = xmltodict.parse(read_file)
-            return formatted_file["dataset"]["record"]
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class ReportTypeStrategy(ABC):
@@ -57,11 +24,19 @@ class Completo(ReportTypeStrategy):
         return CompleteReport.generate(report)
 
 
+importers = [CsvImporter, JsonImporter, XmlImporter]
+
+
 class Inventory:
     @classmethod
     def read_report_file(cls, path: str) -> list[dict]:
-        file_extension = path.split(".")[-1].upper() + "FileReader"
-        formatted_file = eval(file_extension).read(path)
+        importer_strategy = path.split(".")[-1].capitalize() + "Importer"
+        used_importer = [
+            importer
+            for importer in importers
+            if importer_strategy in str(importer)
+        ][0]
+        formatted_file = used_importer.import_data(path)
         return formatted_file
 
     @classmethod
@@ -69,50 +44,3 @@ class Inventory:
         report = cls.read_report_file(path)
         type_class = type.capitalize()
         return eval(type_class).get_report(report)
-
-
-# from abc import ABC, abstractmethod
-# from inventory_report.reports.simple_report import SimpleReport
-# from inventory_report.reports.complete_report import CompleteReport
-# from inventory_report.importer.csv_importer import CsvImporter
-# from inventory_report.importer.json_importer import JsonImporter
-# from inventory_report.importer.xml_importer import XmlImporter
-
-
-# class ReportTypeStrategy(ABC):
-#     @abstractmethod
-#     def get_report(cls, report):
-#         raise NotImplementedError
-
-
-# class Simples(ReportTypeStrategy):
-#     @classmethod
-#     def get_report(cls, report):
-#         return SimpleReport.generate(report)
-
-
-# class Completo(ReportTypeStrategy):
-#     @classmethod
-#     def get_report(cls, report):
-#         return CompleteReport.generate(report)
-
-
-# importers = [CsvImporter, JsonImporter, XmlImporter]
-
-
-# class Inventory:
-#     @classmethod
-#     def read_report_file(cls, path: str) -> list[dict]:
-#         importer_strategy = path.split(".")[-1].capitalize() + "Importer"
-#         used_importer = [
-#             importer for importer
-#             in importers if importer == importer_strategy
-#         ]
-#         formatted_file = used_importer[0].read(path)
-#         return formatted_file
-
-#     @classmethod
-#     def import_data(cls, path: str, type: str):
-#         report = cls.read_report_file(path)
-#         type_class = type.capitalize()
-#         return eval(type_class).get_report(report)
