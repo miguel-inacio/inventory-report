e67a6ddcf9db5acbbf3a6373e1e0e89e214a69ef
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-05 17:07:49 -0300

    feat: cria programa para acessar arquivos e relatórios pela linha de comando

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+importers = [CsvImporter, XmlImporter, JsonImporter]
+
+
 def main():
-    pass
+    try:
+        file_path = sys.argv[1]
+        report_type = sys.argv[2]
+        picked_importer = file_path.split(".")[-1].capitalize() + "Importer"
+        importer_class = [
+            importer
+            for importer in importers
+            if picked_importer in str(importer)
+        ][0]
+        inventory = InventoryRefactor(importer_class)
+        sys.stdout.write(inventory.import_data(file_path, report_type))
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
