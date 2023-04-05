63619051dbdd377e16fc27a02200fb0c859848cf
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-05 11:08:06 -0300

    test: adiciona teste para report decorado

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,37 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+
+    products_list = [
+        {
+            "id": "44",
+            "nome_do_produto": "Papel A4",
+            "nome_da_empresa": "Dunder Mifflin",
+            "data_de_fabricacao": "2005-03-24",
+            "data_de_validade": "2033-08-10",
+            "numero_de_serie": "101",
+            "instrucoes_de_armazenamento": "em lugar seco",
+        },
+        {
+            "id": "2",
+            "nome_do_produto": "Papel sulfite",
+            "nome_da_empresa": "Dunder Mifflin",
+            "data_de_fabricacao": "2005-09-20",
+            "data_de_validade": "2025-02-13",
+            "numero_de_serie": "201",
+            "instrucoes_de_armazenamento": "em lugar seco",
+        },
+    ]
+    report_decorator = ColoredReport(SimpleReport)
+    decorated_report = report_decorator.generate(products_list)
+
+    assert decorated_report == (
+        "\033[32mData de fabricação mais antiga:\033[0m "
+        "\033[36m2005-03-24\033[0m\n"
+        "\033[32mData de validade mais próxima:\033[0m "
+        "\033[36m2025-02-13\033[0m\n"
+        "\033[32mEmpresa com mais produtos:\033[0m "
+        "\033[31mDunder Mifflin\033[0m"
+    )
